37b97f03a1b4821a28100e5387c8ae716a39d32f
Jewelry <supirs102@gmail.com>
2018-06-27 16:07:54 +0900

    background-check導入するも動かず ホバー時の画像変更４つ終了 フルサイズ化は未終了

---
+++ js/main.js
@@ -0,0 +1,22 @@
+$(function(){	
+	$('#page').find('#page-content').find('li:nth-child(1)').find('a').mouseenter(function() {
+		$('#page').css({'background-image':'url("images/back01.jpg")'});
+	}).mouseleave(function() {
+		$('#page').css({'background-image':''});
+	});
+	$('#page').find('#page-content').find('li:nth-child(2)').find('a').mouseenter(function() {
+		$('#page').css({'background-image':'url("images/back02.jpg")'});
+	}).mouseleave(function() {
+		$('#page').css({'background-image':''});
+	});
+	$('#page').find('#page-content').find('li:nth-child(3)').find('a').mouseenter(function() {
+		$('#page').css({'background-image':'url("images/back03.jpg")'});
+	}).mouseleave(function() {
+		$('#page').css({'background-image':''});
+	});
+});
+document.addEventListener('DOMContentLoaded', function () {
+  BackgroundCheck.init({
+    targets: '.target'
+  });
+});
